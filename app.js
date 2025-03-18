02b947a88e2bb82f0dde2e0442156720767edc9c
EunSeo <jpmdyh21@gmail.com>
2025-03-18 09:59:32 +0900

    express 기본예제

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// http 모듈을 가져옵니다.
-const http = require('http');
+// express 모듈을 가져옵니다.
+const express = require('express');
 
-// 서버를 만듭니다.
-const server = http.createServer((req, res) => {
-  // 응답 헤더 설정
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+// express 애플리케이션을 만듭니다.
+const app = express();
 
+// '/' 경로에 대해 요청을 처리합니다.
+app.get('/', (req, res) => {
   // 응답 본문
-  res.end('Hello, World!\n');
+  res.send('Hello, World!');
 });
 
 // 서버가 3000번 포트에서 대기하도록 설정
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 대기 중입니다.');
 });
