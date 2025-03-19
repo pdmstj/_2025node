14a326420e7ec2a8f66622a0d8859d0a5a765694
EunSeo <jpmdyh21@gmail.com>
2025-03-19 11:36:46 +0900

    json 데이터 넘기기

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const express = require('express');
 // express 애플리케이션을 만듭니다.
 const app = express();
 
+app.use(express.json());
+
 // '/' 경로에 대해 요청을 처리합니다.
-app.post('/', (req, res) => {
+app.post('/a', (req, res) => {
   // 응답 본문
-  res.send('Hello, World!'); 
+  res.send(req.body); 
 });
 
 // 서버가 3000번 포트에서 대기하도록 설정
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 대기 중입니다.');
 });
+5
