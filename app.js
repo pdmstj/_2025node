92f97a135df9893c0a5a1f7b04fc9fb81516f4ce
Your Name <your.email@example.com>
2025-03-19 11:36:49 +0900

    json 데이터 넘기기

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const express = require('express');
 // Express 애플리케이션을 생성합니다.
 const app = express();
 
+app.use(express.json());
+
 // 라우팅 설정
 app.post('/swag', (req, res) => {
   // 클라이언트에게 응답 내용 전송
-  res.send('Hello, World!');
+  res.send(req.body);
 });
+// 지우쌤은 swag있고 power있음
 
 // 서버가 3000번 포트에서 요청을 기다리도록 설정
 app.listen(3000, () => {
