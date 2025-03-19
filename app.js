7878c1779a75975ba6d87f58d0075a892f9da071
EunSeo <jpmdyh21@gmail.com>
2025-03-19 11:50:34 +0900

    param 넘기기

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-// express 모듈을 가져옵니다.
+// express 모듈을 가져옴
 const express = require('express');
 
-// express 애플리케이션을 만듭니다.
+// express 애플리케이션을 만듬
 const app = express();
 
 app.use(express.json());
 
-// '/' 경로에 대해 요청을 처리합니다.
-app.post('/a', (req, res) => {
-  // 응답 본문
-  res.send(req.body); 
+//라우팅 설정
+app.get('/a', (req, res) => {
+    const person = req.params.person;
+    res.status(200).send(person);
 });
 
 // 서버가 3000번 포트에서 대기하도록 설정
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 대기 중입니다.');
 });
-5
